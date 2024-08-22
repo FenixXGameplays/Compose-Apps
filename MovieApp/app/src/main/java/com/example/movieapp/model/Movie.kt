0ae13c525cc7376b0a83b146f646d278d5bcd126
Enrique Fernandez <kikefdezarevalillo@gmail.com>
2024-08-22 17:01:53 +0200

    Add movie

--- MovieApp/app/src/main/java/com/example/movieapp/model/Movie.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/model/Movie.kt
@@ -103,6 +103,25 @@ fun getMovies(): List<Movie>{
             ),
             rating = "8.2" // Update rating
         ),
+        
+        Movie(
+            id = "6",
+            name = "Interstellar",
+            director = "Christopher Nolan",
+            year = "2014",
+            genre = "Adventure, Drama, Sci-Fi",
+            actors = "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+            plot = "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+            poster = "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+            images = listOf(
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA3NTEwOTMxMV5BMl5BanBnXkFtZTgwMjMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMzQ5ODE2MzEwM15BMl5BanBnXkFtZTgwMTMyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg4Njk4MzY0Nl5BMl5BanBnXkFtZTgwMzIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BMzE3MTM0MTc3Ml5BMl5BanBnXkFtZTgwMDIyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BNjYzNjE2NDk3N15BMl5BanBnXkFtZTgwNzEyODgxMzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
+            ),
+            rating = "8.6"
+        ),
     )
 
 }
